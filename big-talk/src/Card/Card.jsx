394902f1eb55a6ead8566533bb946878bb8f3888
Shelden Shi <54699492+Sheldenshi@users.noreply.github.com>
2020-11-04 16:28:43 -0800

    from sandbox

---
+++ big-talk/src/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Card.css";
+
+const Card = (props) => (
+  <div className="card-container">
+    <div className="card">
+      <div className="question"> Question </div>
+    </div>
+  </div>
+);
+
+export default Card;
