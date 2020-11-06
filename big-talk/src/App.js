9185a337ed7f6b9fdb167cac6e8f8c5aceb31c6f
Shelden <shelden@berkeley.edu>
2020-11-05 20:35:02 -0800

    s

--- big-talk/src/App.js
+++ big-talk/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
             currentCard: {}
         }
     }
-    componentWillMount() {
+    componentDidMount() {
         const currentCards = this.state.cards;
 
         this.database.on('child_added', snap => {
